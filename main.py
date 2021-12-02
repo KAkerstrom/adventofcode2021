a95eb90a6f042121abd15e33b42fe83cc8bf451a
Kyle Akerstrom <kyle@taxcycle.com>
2021-12-02 09:32:27 -0700

    Slight refactor of main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import day_1.part_2
 
 directory = os.path.abspath(os.path.dirname(__file__))
 
+def GetAndPrintResults(day : int, part : int, input_data : str, func):
+    start = time.perf_counter_ns()
+    result = func(input_data)
+    end = time.perf_counter_ns()
+    print(f'{day}-{part}\t{result}\t{(end-start) / 1_000_000}ms')
+
+
 print('Advent of Code 2021\nKyle Akerstrom\n-------------------')
 with open(f'{directory}\\day_1\\input.txt', 'r') as file:
     contents = file.read()
-    start = time.perf_counter_ns()
-    result = day_1.part_1.Run(contents)
-    end = time.perf_counter_ns()
-    print(f'1-1\t{result}\t{(end-start) / 1_000_000}ms')
-
-    start = time.perf_counter_ns()
-    result = day_1.part_2.Run(contents)
-    end = time.perf_counter_ns()
-    print(f'1-2\t{result}\t{(end-start) / 1_000_000}ms')
+    GetAndPrintResults(1, 1, contents, day_1.part_1.Run)
+    GetAndPrintResults(1, 1, contents, day_1.part_2.Run)
